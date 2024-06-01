dc9eeef9e542cd8cacd594d0b3ccbb7b839cd37c
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-02 00:18:38 +0200

    Refactor config structure in frontend
    This commit introduces a restructured configuration setup in the frontend. It separates the types of configuration into a new types.ts file and renames configFrontend.ts to frontend.ts. This restructuring enhances readability and maintainability by separating concerns and standardizing imports.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import "../app.css";
-	import config from '../configFrontend'
+	import config from '../config'
 	import posthog from 'posthog-js'
 	posthog.init(config.postHog.API_KEY, { api_host: config.postHog.API_HOST })
 	posthog.capture('$pageview')
