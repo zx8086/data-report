bc7be6423dcb08ac59300cea3334542f5a5309e5
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-01 19:54:36 +0200

    Add TailwindCSS theme styles and postCSS plugins
    Added Calvin Klein and Tommy Hilfiger theme styles using TailwindCSS within the frontend, along with accompanying changes in the CSS and configuration files. This involved making modifications to the package.json, postcss.config.js, and tailwind.config.js files. Additionally, a few postCSS plugins were included to facilitate the nesting of rules and importing styles.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,12 @@
+// postcss.config.js
 export default {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
-  },
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
+  }
 }
