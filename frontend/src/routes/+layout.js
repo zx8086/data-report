ef14b1f9666726c2914e00c27afb17aea7383d90
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-01 17:52:12 +0200

    Refactor tracking code and relocate layout file
    Posthog import and pageview capture code has been moved to +layout.svelte script bloc from various JavaScript and Svelte files. +layout.js has been commented out for now, and may be removed in future if not needed. Also, renamed and moved +layout.js to the routes directory.

---
+++ frontend/src/routes/+layout.js
@@ -0,0 +1,16 @@
+// import posthog from 'posthog-js'
+// import { browser } from '$app/environment';
+//
+// export const load = async () => {
+//
+// 	if (browser) {
+// 		posthog.init(
+// 			'phc_qmqwPLsWmycDSEzZqEYoYVj4fVl8JuEO8wjaxTfezTh',
+// 			{
+// 				api_host: 'https://eu.i.posthog.com',
+//
+// 			}
+// 		)
+// 	}
+// 	return
+// };
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import "../app.css";
+	import posthog from 'posthog-js'
+	posthog.init('phc_qmqwPLsWmycDSEzZqEYoYVj4fVl8JuEO8wjaxTfezTh', { api_host: "https://eu.i.posthog.com" })
+	posthog.capture('$pageview')
 </script>
 
 <slot />
