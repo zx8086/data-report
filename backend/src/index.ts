6683d7ef24d549682bd72c44102dd5e092b98634
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-01 22:20:43 +0200

    Add CORS middleware and modify TypeScript module resolution
    This commit introduces CORS middleware to the Elysia app and changes module resolution setting in TypeScript from 'node' to 'bundler'. Additionally, it adds new dependencies for CORS and response cache in package.json. The healthCheckEndpoint is set to '/live' and the response cache plugin is commented out for potential future use.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,23 @@
 import {Elysia} from 'elysia';
+import { cors } from '@elysiajs/cors'
 
 import {yoga} from '@elysiajs/graphql-yoga';
 import typeDefs from './graphql/schema/typeDefs';
 import resolvers from './graphql/schema/resolvers';
+import { useResponseCache } from '@graphql-yoga/plugin-response-cache';
 
-const app = new Elysia().use(yoga({logging: 'debug', typeDefs, resolvers})).listen(4000);
+const app = new Elysia()
+	.use(cors())
+	.use(yoga({
+		logging: 'debug',
+		healthCheckEndpoint: '/live',
+		typeDefs,
+		resolvers,
+		// plugins: [
+		// 	useResponseCache({
+		// 		// global cache
+		// 		session: () => null,
+		// 	}),
+		// ],
+	}),
+).listen(4000);
