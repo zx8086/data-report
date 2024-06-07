ee7eaefce85636235e4be7acdc8b1d8e61c9da04
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-07 04:16:04 +0200

    Refactor code in index.ts for cleaner syntax
    In the `backend/src/index.ts` file, the unnecessary repetition of `typeDefs` and `resolvers` parameters in the `yoga` function has been removed for cleaner and more concise syntax. This code simplification improves readability and maintainability.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,8 +6,8 @@ import resolvers from './graphql/resolvers';
 const app = new Elysia()
 	.use(
 		yoga({
-			typeDefs:typeDefs,
-			resolvers: resolvers
+			typeDefs,
+			resolvers
 		}))
 	.listen(4000)
 console.log("GraphQL server running on port:4000");
