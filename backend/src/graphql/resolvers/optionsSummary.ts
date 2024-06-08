3833cce2d6fd22c4ae3af74702793ee112619abb
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-08 13:37:11 +0200

    Add optionsSummary to GraphQL schema and resolvers
    A new type 'OptionSummary' and related resolver method are added to the GraphQL schema and resolver respectively. This new option allows to query various attributes of product options such as availability, status, and updates. In addition, a new enumerator 'SalesChannel' was also included for better options filtering.

---
+++ backend/src/graphql/resolvers/optionsSummary.ts
@@ -0,0 +1,33 @@
+// optionsSummary.ts
+import { getCluster } from "../../lib/clusterProvider";
+const optionsSummary = {
+	Query: {
+		optionsSummary: async (_: unknown, args: {
+			SalesOrganizationCode: string,
+			StyleSeasonCode: string,
+			DivisionCode: string,
+			ActiveOption: boolean,
+			SalesChannels: string[]
+		}): Promise<any> => {
+			try {
+				const { SalesOrganizationCode, StyleSeasonCode, DivisionCode, ActiveOption, SalesChannels } = args;
+				const cluster = await getCluster();
+				const query = `EXECUTE FUNCTION \`default\`.\`_default\`.get_options_summary($SalesOrganizationCode, $StyleSeasonCode, $DivisionCode, $ActiveOption, $SalesChannels)`;
+				const queryOptions = {
+					parameters: {
+						SalesOrganizationCode, StyleSeasonCode, DivisionCode, ActiveOption, SalesChannels
+					},
+				};
+				console.log("Query", query);
+				console.log("queryOptions", queryOptions);
+				let result = await cluster.cluster.query(query, queryOptions);
+				console.log(JSON.stringify(result, null, 2));
+				return result.rows[0][0];
+			} catch (error) {
+				console.error('Error:', error);
+				throw error;
+			}
+		},
+	},
+};
+export default optionsSummary;
