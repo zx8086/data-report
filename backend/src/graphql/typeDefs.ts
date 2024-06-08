0cf09e059edc1c5a29de3ab007b04f38f2369adf
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-08 02:48:00 +0200

    Add new types to GraphQL typeDefs
    A substantial number of new types have been added to the GraphQL type definition. These new types include 'Status', 'Size', 'SizeSet', 'Inseam', 'SizesMap', 'Dimensions', and 'Option' among others. The additions are designed to support a broadened set of GraphQL queries.

--- backend/src/graphql/typeDefs.ts
+++ backend/src/graphql/typeDefs.ts
@@ -23,6 +23,156 @@ const typeDefs = gql`
         totalLooks: Int
     }
 
+    type Status {
+        code: String
+        description: String
+        isAvailable: Boolean
+        isCancelled: Boolean
+        isClosed: Boolean
+        isInvalid: Boolean
+        isSoldOut: Boolean
+    }
+
+    type Size {
+        EAN: String
+        index: Int
+        name: String
+        pimVariantKeyNo: String
+        sapVariant: String
+        status: Status
+    }
+
+    type SizeSet {
+        sizes: [Size]
+    }
+
+    type Inseam {
+        null: SizesMap
+    }
+
+    type SizesMap {
+        sizes: [Size]
+    }
+
+    type Dimensions {
+        heelHeight: Int
+        height: Int
+        length: Int
+        shaftHeight: Int
+        width: Int
+    }
+
+    type Theme {
+        name: String
+    }
+
+    type PimDeliveryDates {
+        additionalDeliveryDate: String
+        adjustedDeliveryDate: String
+        dropDate: Float
+        modifiedOn: String
+    }
+
+    type MerchandisingHierarchy {
+        business: String
+        category: String
+        code: String
+        gender: String
+        lifestyle: String
+        productgroup: String
+        productsubgroup: String
+        world: String
+    }
+
+    type Option {
+        activeOption: Boolean
+        brandCode: String
+        certificationStyles: [String]
+        channels: [String]
+        collection: String
+        collectionStructure: String
+        color: String
+        colorDescription1: String
+        colorDescription2: String
+        consumerAssortment: String
+        consumerAssortmentCode: String
+        copiedFromOption: String
+        countryOfOrigin: String
+        ctpMonth: String
+        description: String
+        descriptions: String
+        dimensions: Dimensions
+        distribution: String
+        divisionCode: String
+        documentType: String
+        fabric: String
+        fashionability: String
+        fashionabilityCode: String
+        fit: String
+        fitForBottoms: String
+        fitForTops: String
+        fmsCollectionCode: String
+        fmsMainSeasonCode: String
+        garmentLength: String
+        hasSustainableMaterials: Boolean
+        hexCode: String
+        images: String
+        innovation: Boolean
+        innovationAttributes: String
+        inseams: Inseam
+        internalStyleName: String
+        internal_id: String
+        isAvailable: Boolean
+        isCancelled: Boolean
+        isClosed: Boolean
+        isInvalid: Boolean
+        isLicensed: Boolean
+        isNew: Boolean
+        isSoldOut: Boolean
+        isUpdated: Boolean
+        keyLook: String
+        label: String
+        llo: Int
+        mainSeason: String
+        merchandisingHierarchy: MerchandisingHierarchy
+        neckline: String
+        openForEcom: Boolean
+        optionCode: String
+        optionType: String
+        originatingProductLine: String
+        originatingProductLineCode: String
+        packingType: String
+        pimDeliveryDates: PimDeliveryDates
+        pimOptionKeyNo: String
+        pimProductKeyNo: String
+        prepackSizeOffer: String
+        priceBySize: Boolean
+        productStory: String
+        program: String
+        referenceOptionNumber: String
+        referenceStyleNumber: String
+        rise: String
+        salesChannels: [String]
+        salesComments: String
+        salesOrganizationCode: String
+        seasonYear: String
+        shape: String
+        sizeCode: String
+        sizeRange: String
+        sizeRangeOption: String
+        sleeveLength: String
+        styleDescription: String
+        styleDescriptions: [String]
+        styleNumber: String
+        styleSeasonCode: String
+        sustainableAttribute: String
+        sustainableFiber: String
+        sustainableFibers: [String]
+        swatchBookNumber: String
+        theme: Theme
+        wash: String
+    }
+    
     type Query {
         looksSummary(brand: String, season: String, division: String): LookSummary
         looks(brand: String, season: String, division: String): [Look]
