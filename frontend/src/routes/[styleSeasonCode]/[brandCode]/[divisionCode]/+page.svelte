b98a3700b542db815f03ba4b5dc55c79ddc68a50
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-10 21:58:21 +0200

    Refactor frontend code for displaying look summaries
    The code for displaying look summaries has been cleaned up and streamlined. Unnecessary if statements and console logs have been removed from the `+page.svelte` file. Additionally, the ApolloClient is created through a function in `+page.server.ts` for better modularity, and the response error handling has been improved. Minor text formatting change has also been made in ProgressBar component.

--- frontend/src/routes/[styleSeasonCode]/[brandCode]/[divisionCode]/+page.svelte
+++ frontend/src/routes/[styleSeasonCode]/[brandCode]/[divisionCode]/+page.svelte
@@ -3,75 +3,36 @@
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
 
 	export let data: LooksSummary;
-
-	console.log("Passed Data", data)
-
-	let divisionName = "Kids";
+	let divisionName = 'Kids';
 </script>
 
 <div class="container ml-20 mx-auto p-0">
 	<div class="flex flex-row justify-center space-x-4">
-		<div class="w-1/2"></div>
+		<div class="w-1/2">Collections</div>
 
 		<div class="w-1/2">
-			<div class="card-th mb-4">
-				<h2 class="heading-th p-4">{divisionName} LOOKS</h2>
+			<div class="card-th">
+				<h2 class="heading-th p-4">LOOKS</h2>
 				<div class="p-4">
-					<p class="font-bold">LOOKS</p>
-					<p>{data.hasTitle} OF {data.totalLooks} COMPLETE</p>
-
-					<p>
-						<ProgressBar
-							percentage={((data.totalLooks - data.hasTitle) / data.totalLooks) * 100}
-							completed={data.hasTitle}
-							total={data.totalLooks}
-						/>
+					<p class="font-bold">{divisionName} LOOKS</p>
+					<p>Gender
+						<ProgressBar percentage={Math.round((data.hasGender / data.totalLooks) * 100)} completed={data.hasGender} total={data.totalLooks} />
 					</p>
-					{#if data.hasDescription}
-						<p>
-							<ProgressBar
-								percentage={(data.hasDescription / data.totalLooks) * 100}
-								completed={data.hasDescription}
-								total={data.totalLooks}
-							/>
-						</p>
-					{/if}
-					{#if data.hasGender}
-						<p>
-							<ProgressBar
-								percentage={(data.hasGender / data.totalLooks) * 100}
-								completed={data.hasGender}
-								total={data.totalLooks}
-							/>
-						</p>
-					{/if}
-					{#if data.hasRelatedStyles}
-						<p>
-							<ProgressBar
-								percentage={(data.hasRelatedStyles / data.totalLooks) * 100}
-								completed={data.hasRelatedStyles}
-								total={data.totalLooks}
-							/>
-						</p>
-					{/if}
-					{#if data.hasTag}
-						<p>
-							<ProgressBar
-								percentage={(data.hasTag / data.totalLooks) * 100}
-								completed={data.hasTag}
-								total={data.totalLooks}
-							/>
-						</p>
-					{/if}
-					{#if data.hasTrend}
-						<p>
-							<ProgressBar
-								percentage={(data.hasTrend / data.totalLooks) * 100}
-								completed={data.hasTrend}
-								total={data.totalLooks}
-							/>
-						</p>
-					{/if}
+					<p>Tags
+						<ProgressBar percentage={Math.round((data.hasTag / data.totalLooks) * 100)} completed={data.hasTag} total={data.totalLooks} />
+					</p>
+					<p>Description
+						<ProgressBar percentage={Math.round((data.hasDescription / data.totalLooks) * 100)} completed={data.hasDescription} total={data.totalLooks} />
+					</p>
+					<p>Trends
+						<ProgressBar percentage={Math.round((data.hasTrend / data.totalLooks) * 100)} completed={data.hasTrend} total={data.totalLooks} />
+					</p>
+					<p>Related Styles
+						<ProgressBar percentage={Math.round((data.hasRelatedStyles / data.totalLooks) * 100)} completed={data.hasRelatedStyles} total={data.totalLooks} />
+					</p>
+					<p>Title
+						<ProgressBar percentage={Math.round((data.hasTitle / data.totalLooks) * 100)} completed={data.hasTitle} total={data.totalLooks} />
+					</p>
 				</div>
 			</div>
 		</div>
