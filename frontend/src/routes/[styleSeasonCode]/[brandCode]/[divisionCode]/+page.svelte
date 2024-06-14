fa435b8c7be7e29e2d83d74d947ea14865601cbe
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-06-14 15:29:46 +0200

    Add video controls and update video sources
    Added controls to the video element and removed hardcoded source path, now referring directly to the look asset. Furthermore, expanded the videoMap to include more video URLs, and set up a reactive statement to update the video source based on the current page's URL, thereby making the video display more dynamic and robust.

--- frontend/src/routes/[styleSeasonCode]/[brandCode]/[divisionCode]/+page.svelte
+++ frontend/src/routes/[styleSeasonCode]/[brandCode]/[divisionCode]/+page.svelte
@@ -2,6 +2,7 @@
 	import posthog from 'posthog-js';
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
 	import DivisionalVideoPlayer from '$lib/components/DivisionalVideoPlayer.svelte';
+	import {page} from '$app/stores';
 
 	posthog.capture('$pageview')
 
@@ -13,16 +14,44 @@
 		let videoMap = {
 			'01': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/TH%20SP25%20MSW%20TRAILER_SP25_1920x1080_C5101`,
 			'02': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/TH_SP25_MASH%20UP_JEANS_C5102`,
-			'03': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/TH_SP25_MASH%20UP_JEANS_C5102`
+			'03': ``,
+			'04': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/HILFIGER_KIDS_SP25_1920x1080_C5104`,
+			'05': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/HILFIGER_WSW%20TRAILER_SP25_1920x1080_C5105`,
+			'06': ``,
+			'07': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/HILFIGER_CTB_SP25_1920x1080_C5107`,
+			'08': ``,
+			'09': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/WSW_FTW-ACC_SP25_1920x1080_C5109`,
+			// '09': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/MSW_FTW_ACC_SP25_1920x1080_C5109`,
+			'10': ``,
+			'61': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKMenswear_sizzle_C5161`,
+			'62': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKJeans_sizzle_C5162`,
+			'63': ``,
+			'64': ``,
+			'65': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKWomenswear_sizzle_C5165`,
+			'66': ``,
+			'67': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKUnderwear_sizzle_C5167`,
+			'68': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKSport_sizzle_C5168`,
+			'69': ``,
+			'70': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKAccessories_sizzle_C5170`,
+			'77': `https://s7g10.scene7.com/is/content/TommyHilfigerEU/CKSP25_CKSwimwear_sizzle_C5177`,
+			'97': ``
 		}
 		// You can set a default URL as well
-		return videoMap[divisionCode]
+		return videoMap[divisionCode]  || videoMap['01']; // default video when there is no videoMap for current `divisionCode`
 	}
 
-	let video = `https://s7g10.scene7.com/is/content/TommyHilfigerEU/TH%20SP25%20MSW%20TRAILER_SP25_1920x1080_C5101`;
+	// let video = `https://s7g10.scene7.com/is/content/TommyHilfigerEU/TH%20SP25%20MSW%20TRAILER_SP25_1920x1080_C5101`;
 
-	console.log("Video",video)
-
+	let video: any = null;
+	$: {
+		if ($page && $page.url) {
+			// a reactive statement to update the video based on the page URL
+			const divisionCode = $page.url.pathname.split('/')[3];  // using `split` to get the division code from the URL.
+			video = getVideoForDivision(divisionCode);
+			console.log("Division Code",divisionCode)
+			console.log("Video",video);
+		}
+	}
 </script>
 
 <div class="container ml-20 mx-auto p-0">
